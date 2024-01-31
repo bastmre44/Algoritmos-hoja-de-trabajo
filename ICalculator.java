4ae9f526993c3a81198e878e2e6706bf46b85d83
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2024-01-30 22:05:55 -0600

    Update ICalculator.java

--- ICalculator.java
+++ ICalculator.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+//Interfaz para la calculadora 
 
 public interface ICalculator {
 
+
+    //métodos que se van a usar en la calculadora , suma, resta, multiplicación y división 
     public int add(int n1, int n2); // +
 
     public int substraction(int n1, int n2); // -
